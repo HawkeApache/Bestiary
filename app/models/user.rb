66b468d8181b81d991a1d6578aace5c7c13f4774
Adrian Lato <adrian.lato@onet.pl>
2018-06-08 14:15:41 +0200

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :comments
-  has_and_belongs_to_many :subjects
 
   after_create :set_sub
 
